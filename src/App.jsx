edde60c6e5bdd3c6efe5f395181bc116e4b5f88c
Durvesh Pokade <durveshpokade835@gmail.com>
2024-04-04 16:19:00 +0530

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DataContainer from './components/DataContainer';
+
+function App() {
+
+
+  return (
+    <div>
+      <DataContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/DataContainer.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Card, Container, ListGroup } from 'react-bootstrap';
+
+export default function DataContainer() {
+    const [data, setData] = useState([
+        {
+            id: 1,
+            name: 'Durvesh Pokade',
+            age: 24,
+        },
+        {
+            id: 2,
+            name: 'Ekaksh Pawar',
+            age: 23,
+        },
+        {
+            id: 3,
+            name: 'Yogesh Peje',
+            age: 22,
+        },
+        {
+            id: 4,
+            name: 'Shardul Bhuvad',
+            age: 30,
+        },
+    ]);
+
+    return (
+        <Container style={{ textAlign: 'center' }}>
+            <h1>List of Students</h1>
+            <ListGroup as="ul"  >
+                {data.map(items => (
+                    <ListGroup.Item variant="dark" >
+                        <span>{items.name}</span> Age :<span>{items.age}</span>
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+        </Container>
+    );
+};
